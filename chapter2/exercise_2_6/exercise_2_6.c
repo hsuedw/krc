82ed87bf4c215b92e0d4491204e1cb610d9a9905
edward.hc.hsu <hsuedw@gmail.com>
2022-08-12 16:24:06 +0800

    Implement the solution for Exercise 2-6

--- chapter2/exercise_2_6/exercise_2_6.c
+++ chapter2/exercise_2_6/exercise_2_6.c
@@ -1,4 +1,21 @@
+#include <stdio.h>
+
 unsigned int setbits(unsigned int x, int p, int n, unsigned int y)
 {
-    return (x & ~(~(~0 << n)  << (p + 1 - n))) | (y &   ~(~0 << n)) << (p + 1 - n);
+    unsigned int y_mask = ~(~0 << n);
+    unsigned int shift = p + 1 - n;
+    unsigned int x_mask = ~(~(~0 << n) << shift);
+
+    return ((y & y_mask) << shift) | (x & x_mask);
+}
+
+int main(int argc, char *argv[])
+{
+    unsigned int x = 0x12345678;
+    unsigned int y = 0xabcdef01;
+
+    printf("x: 0x%x, y: 0x%x\n", x, y);
+    printf("Before: 0x%x, After: 0x%x\n", x, setbits(x, 30, 20, y));
+
+    return 0;
 }
